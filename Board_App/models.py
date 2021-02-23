aad6241f011420f106de1f04a57b01cbe063a451
Masaya00 <masaya.asxce@icloud.com>
2021-02-23 17:55:27 +0900

    edit models.py

--- Board_App/models.py
+++ Board_App/models.py
@@ -3,22 +3,27 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    created_at = models.DateTimeField(default=timezone.now)
+    # 必須 / デフォルトに現在日時を指定
+    created_at = models.DateTimeField('登録日時', default=timezone.now)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-created_at']
+        verbose_name = 'スレッド'
+        verbose_name_plural = 'スレッド'
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
+    # スレッドにコメントを紐付け。　スレッドが削除されればコメントも削除
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.CharField(max_length=50)
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    # 必須 / デフォルトに現在日時を指定
+    created_at = models.DateTimeField('登録日時', default=timezone.now)
 
     def __str__(self):
         return self.text
 
     class Meta:
-        ordering = ['-created_at']
+        verbose_name = 'コメント'
+        verbose_name_plural = 'コメント'
